ed640d5f80ecc648f57d95acda2137638f45d018
Abdul Faiq <abdulfaiq47@gmail.com>
2026-03-16 14:40:04 +0500

    Add files via upload

--- frontend/api/hello.py
+++ frontend/api/hello.py
@@ -2,11 +2,11 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # allows your Next.js frontend to fetch
+CORS(app)
 
 @app.route("/api/hello")
 def hello():
     return jsonify({"message": "Hello from Flask Serverless!"})
 
-# Important for Vercel
+# MUST HAVE
 handler = app
